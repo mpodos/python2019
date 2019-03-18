d74444b73c51ffce2158504f458afa56fe71dfda
Maria Podosinnikova <maramie@yandex.ru>
2019-03-18 13:20:44 +0300

    Fix color

--- hw.py
+++ hw.py
@@ -4,6 +4,6 @@ def button_exit():
     exit(0)
 root=Tk()
 
-button = Button(root, bg="lilac", text="Exit", command=button_exit)
+button = Button(root, bg="silver", text="Exit", command=button_exit)
 button.pack()
 root.mainloop()
